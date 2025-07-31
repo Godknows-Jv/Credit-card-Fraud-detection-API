b19ddf70b9f9640110fe540a73b77b3bf4cb8640
Godknows-Jv <Godknows.Nyandoro@students.uz.ac.zw>
2025-07-31 16:37:41 +0200

    Adding the online url o the API

--- test_api.py
+++ test_api.py
@@ -2,11 +2,13 @@ import requests
 import json
 
 # Load JSON data
-with open("test_data3.json", "r") as file:
+with open("test_data2.json", "r") as file:
     data = json.load(file)
 
+
+# online link to the API https://credit-card-fraud-detection-api-hsrz.onrender.com
 # Send POST request to your local API
-response = requests.post("http://127.0.0.1:5000/predict", json=data)
+response = requests.post("https://credit-card-fraud-detection-api-hsrz.onrender.com/predict", json=data)
 
 # Show the response
 print("Status Code:", response.status_code)
